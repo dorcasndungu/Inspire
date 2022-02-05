4f20f853dc7bc552710fc07f50826ca757e3169d
dorcasndungu <ndungumwihaki017@gmail.com>
2022-02-06 01:24:39 +0300

    this adds the highlight property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,9 @@
 export class Quote {
   showDetails:boolean;
-  constructor(public id:number,public quotation: string,public author: string,public submitter: string, public completeDate: Date,public upVotes:Number,public downVotes:Number){
+  highlight:boolean;
+  constructor(public id:number,public quotation: string,public author: string,public submitter: string, public completeDate: Date,public upVotes:number,public downVotes:number){
     this.showDetails=false;
+    this.highlight=false;
 
 }
 }
