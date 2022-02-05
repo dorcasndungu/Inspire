9ced98f97654ebc32c931448dc3c12164bcab337
dorcasndungu <ndungumwihaki017@gmail.com>
2022-02-05 13:57:51 +0300

    this adds the initial votes properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,7 @@
 export class Quote {
   showDetails:boolean;
-  constructor(public id:number,public quotation: string,public author: string,public submitter: string, public completeDate: Date){
+  constructor(public id:number,public quotation: string,public author: string,public submitter: string, public completeDate: Date,public upVotes:Number,public downVotes:Number){
     this.showDetails=false;
+
 }
 }
